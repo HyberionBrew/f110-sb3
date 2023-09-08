fixed various bugs and switched to using acceleration and normalization coming from dataset

base_env.py
@@ -279,12 +279,12 @@ class NormalizeVelocityObservation(gym.ObservationWrapper):
         return obs
 
 class NormalizePose(gym.ObservationWrapper):
-    def __init__(self, env, low=-30, high=30):
+    def __init__(self, env, low=-30, high=30, theta_low= -np.pi*2, theta_high=np.pi*2):
         super().__init__(env)
         self.pose_bounds = {
             'poses_x': (low, high),
             'poses_y': (low, high),
-            'poses_theta': (-np.pi*2, np.pi*2)
+            'poses_theta': (theta_low, theta_high)
         }
         for key, (low, high) in self.pose_bounds.items():
             self.observation_space[key] = Box(shape=(1,), low=-1.0, high=1.0)
@@ -307,8 +307,11 @@ class NormalizePreviousAction(gym.ObservationWrapper):
     def observation(self, obs):
         low = self.env.action_space.low
         high = self.env.action_space.high
+        # print(high)
         #print(low, high)
         #print("obs", obs["previous_action"])
+        
+        
         obs["previous_action"] = np.clip(obs["previous_action"], low, high)
         obs["previous_action"] = normalize(obs["previous_action"], low, high)
         #print("obs", obs["previous_action"])
@@ -357,7 +360,26 @@ class AccelerationAndDeltaSteeringWrapper(gym.ActionWrapper):
         # print(self.current_velocity)
         # Return the transformed action (absolute values)
         return np.asarray([[self.current_steering, self.current_velocity]])
-    
+
+from f110_orl_dataset import normalize_dataset
+
+class NormalizeObservations(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.normalizer = normalize_dataset.Normalize()
+        lidar_space = self.observation_space["lidar_occupancy"]
+        self.observation_space = self.normalizer.new_state_space
+        self.observation_space["lidar_occupancy"] = lidar_space
+    def observation(self, obs):
+        #print("og")
+        #print(obs)
+        obs = self.normalizer.normalize_obs_batch(obs)
+        #print(obs)
+        #print("new")
+        #print(obs)
+        return obs
+
+
 
 class ReduceSpeedActionSpace(gym.ActionWrapper):
     def __init__(self,env, low, high):
@@ -389,7 +411,8 @@ standard_config = {
     "normalize": False,
 }
 
-from config import VEL_LOW, VEL_HIGH, POSE_LOW, POSE_HIGH, SUBSAMPLE
+from config import VEL_LOW, VEL_HIGH, MAX_VEL, MIN_VEL, \
+                    POSE_LOW, POSE_HIGH, SUBSAMPLE, POSE_THETA_LOW, POSE_THETA_HIGH
 
 def make_base_env(map= "Infsaal", fixed_speed=None, 
                   random_start=True,
@@ -418,7 +441,7 @@ def make_base_env(map= "Infsaal", fixed_speed=None,
 
     # Clip the velocity
     # env = ReduceSpeedActionSpace(env, 0.5, 1.8)
-    env = ReduceSpeedActionSpace(env, 0.7, 2.0)
+    env = ReduceSpeedActionSpace(env, MIN_VEL, MAX_VEL)
     env = ClipAction(env)
 
     # restart if start spinning
@@ -427,20 +450,17 @@ def make_base_env(map= "Infsaal", fixed_speed=None,
     # only steering is learnable
     if fixed_speed is not None:
         env = FixSpeedControl(env, fixed_speed=fixed_speed)
-    print(env.action_space.low[0])
+    # print(env.action_space.low[0])
     
     # make it 20 HZ from 100 HZ
     env = FrameSkip(env, skip=5) 
     env = MixedGymReward(env, **reward_config)
-    print(env.action_space)
+    # print(env.action_space)
     env = AppendActionToInfo(env, name="action_raw")
     env = AugmentObservationsPreviousAction(env, inital_velocity=0.7)
-
+    
     # convert to delta steering and acceleration
-    env = AccelerationAndDeltaSteeringWrapper(env, max_acceleration=0.05, max_delta_steering=0.05,
-                                              min_velocity=env.action_space.low[0][1], max_velocity=env.action_space.high[0][1],
-                                              min_steering=env.action_space.low[0][0], max_steering=env.action_space.high[0][0],
-                                              inital_velocity=0.7)
+
     # special wrapper for evaluation
     if eval:
         env = MaxLaps(env, max_laps=1, finished_reward=20, max_lap_time=1000, use_org_reward=use_org_reward)
@@ -456,15 +476,23 @@ def make_base_env(map= "Infsaal", fixed_speed=None,
 
     # append all observations to the info dict
     env = AppendObservationToInfo(env)
+    
+
+    # Normalize the whole observation space
+    env = NormalizeObservations(env)
+    #env = NormalizePreviousAction(env)
+    #env = NormalizeVelocityObservation(env, low_vel=VEL_LOW, high_vel=VEL_HIGH)
+    #env = NormalizePose(env, low=POSE_LOW, high=POSE_HIGH, 
+    #                   theta_low=POSE_THETA_LOW, theta_high=POSE_THETA_HIGH)
+    # Change action space
+    env = AccelerationAndDeltaSteeringWrapper(env, max_acceleration=0.05, max_delta_steering=0.05,
+                                            min_velocity=env.action_space.low[0][1], max_velocity=env.action_space.high[0][1],
+                                            min_steering=env.action_space.low[0][0], max_steering=env.action_space.high[0][0],
+                                            inital_velocity=0.7)
     env = AppendActionToInfo(env, name="action_delta")
-    
-    
-    # Normalize action and observation space
-    env = NormalizeVelocityObservation(env, low_vel=VEL_LOW, high_vel=VEL_HIGH)
-    env = NormalizePose(env, low=POSE_LOW, high=POSE_HIGH)
-    env = NormalizePreviousAction(env) # TODO!
     env = RescaleAction2(env, min_action=-1.0,max_action=1.0)
     
+    env = ClipAction(env)
     # add a timelimit TODO! maybe not include here
     env = TimeLimit(env, max_episode_steps=5000)
     return env

collect_rollouts.py
@@ -2,7 +2,7 @@ import gymnasium
 from argparse import Namespace
 import yaml
 import numpy as np
-
+import torch
 import pickle as pkl
 from absl import flags, app
 
@@ -57,7 +57,7 @@ def main(args):
     model_name = args.model_name
     episode = 0
     timesteps = 0
-    with open(f"datasets4/{args.model_name}", 'wb') as f:
+    with open(f"datasets5/{args.model_name}", 'wb') as f:
         pass
 
 
@@ -76,27 +76,51 @@ def main(args):
         actions = []
         steerings = []
         vels = []
+        tensor_obs = model.policy.obs_to_tensor(obs)[0]
+        #print("...")
+        #print(action)
+        #print(tensor_obs)
+        with torch.no_grad():
+            print(tensor_obs)
+            action, _, log_prob = model.policy.forward(tensor_obs, deterministic=True)# [0]
+        obs, reward, done, truncated, info = eval_env.step(action)
         while not done and not truncated:
             # print(obs)
             timesteps += 1
             # remove key poses_theta from obs
             # del obs["poses_theta"] # TODO! remove bandaid
-            action, _ = model.predict(obs)
-
+            # action, _ = model.predict(obs) #deterministic=True)
+            tensor_obs = model.policy.obs_to_tensor(obs)[0]
+            #print("...")
+            #print(action)
+            #print(tensor_obs)
+            with torch.no_grad():
+                print(tensor_obs)
+                action, _, log_prob = model.policy.forward(tensor_obs, deterministic=True)# [0]
+            print(action)
+            action = action.squeeze(0).detach().numpy()
+            log_prob = float(log_prob.detach().numpy()[0])
             # print(action)
+
+            if args.record:
+                # record values into zarr directory
+                # print(info["observations"])
+                # exit()
+                with open(f"datasets5/{args.model_name}", 'ab') as f:
+                    #if timesteps > args.timesteps:
+                    #    done = True
+                    #    truncated = True
+                    pkl.dump((action, info["observations"], float(reward), done, truncated, log_prob, timesteps, model_name, info["collision"]), f)
+
+
+
             obs, reward, done, truncated, info = eval_env.step(action)
             # print(obs)
             # print(info)
             steerings.append(info["action_raw"][0][0])
             vels.append(info["action_raw"][0][1])
-            #print(actions)
+            # print(action)
             rewards.append(reward)
-            if args.record:
-                # record values into zarr directory
-                # print(info["observations"])
-                # exit()
-                with open(f"datasets4/{args.model_name}", 'ab') as f:
-                    pkl.dump((info["action_delta"], info["observations"], float(reward), done, truncated, info, timesteps, model_name, info["collision"]), f)
 
             if args.render:
                 eval_env.render()
@@ -110,7 +134,9 @@ def main(args):
                     plt.show()
                     plt.plot(rewards)
                     plt.show()
-        
+            # TODO! remove this
+            #if timesteps > args.timesteps:
+            #    break
         # end = time.time()
         #print("Time:", end-start)
 if __name__ == "__main__":

evaluate.py
@@ -11,7 +11,10 @@ parser.add_argument('--fixed_speed', type=float, default=None, help='Fixing the 
 
 args = parser.parse_args()
 import matplotlib.pyplot as plt
-
+import f110_gym
+import f110_orl_dataset
+import gymnasium as gym
+from f110_orl_dataset import normalize_dataset
 
 standard_config = {
     "collision_penalty": -50.0,
@@ -25,9 +28,26 @@ standard_config = {
     "normalize": False,
 }
 
-
+obs_dictionary_keys = [
+    "poses_x",
+    "poses_y",
+    "poses_theta",
+    "ang_vels_z",
+    "linear_vels_x",
+    "linear_vels_y",
+    "previous_action",
+    "progress"
+]
+import numpy as np
 
 def evaluate(args):
+    dataset = gym.make('f110_with_dataset-v0',
+    # only terminals are available as of tight now 
+        **dict(name='f110_with_dataset-v0',
+            config = dict(map="Infsaal", num_agents=1,
+            params=dict(vmin=0.5, vmax=2.0)),
+              render_mode="human")
+    )
     eval_env = make_base_env(map= args.track,
                     fixed_speed=args.fixed_speed,
                     random_start =True,
@@ -37,6 +57,7 @@ def evaluate(args):
     
     model = PPO.load(args.model_path, env=eval_env)
     episode = 0
+    normalizer = normalize_dataset.Normalize()
     while episode < 2000:
         episode += 1
         obs, _ = eval_env.reset()
@@ -58,9 +79,39 @@ def evaluate(args):
             #print(info)
             # print(info)
             # print(action)
-            print("obs", obs)
+            data_dict = info
+            arrays_to_concat = [data_dict['observations'][key].reshape([data_dict['observations'][key].shape[0], -1]) for key in obs_dictionary_keys]
+    
+            # print the shapes of all arrays to concatenate
+            # print([arr.shape for arr in arrays_to_concat])
+            # Concatenate all arrays along the last dimension
+            # remains to test laser
+            concatenated_obs = np.concatenate(arrays_to_concat, axis=-1)
+            
+            concatenated_obs = np.concatenate([concatenated_obs, concatenated_obs])
+            print(concatenated_obs)
+            print(concatenated_obs.shape)
+            print("#####")
+            print(done)
+            print(obs)
+            unfl = normalizer.unflatten_batch(concatenated_obs)
+            re_normalized = normalizer.normalize_obs_batch(unfl)
+            # print(dataset.sim)
+            laser_scans = dataset.get_laser_scan(concatenated_obs, 20)
+            print(laser_scans.shape)
+            normalized_laser_scans = normalizer.normalize_laser_scan(laser_scans)
+            print(normalized_laser_scans[0])
+            print(obs.keys())
+            print(obs["lidar_occupancy"])
+            # There is a bug, probably in F110 env, that causes the theta to be wrong on crash/done
+            assert  done or np.isclose(obs["lidar_occupancy"], normalized_laser_scans[0], rtol=0.15).all()
+            #print(unfl)
+            #print("++++++++++++")
+            
+            #print("obs", obs)
+            # print("info", concatenated_obs)
             # print(eval_env.observation_space)
-            rewards.append(obs["poses_theta"])
+            rewards.append(reward)
             # print("obs reward", reward)
             rew += reward * 0.99 ** steps
             if truncated:

process_datasets.py
@@ -52,6 +52,9 @@ def main(args):
                                     overwrite=False, maxshape=(None, 1))
         collision_array = root.zeros("collision", shape=(0, 1), chunks=(chunks_size, 1), dtype='bool',
                                     overwrite=False, maxshape=(None, 1))
+        log_prob_array = root.zeros("log_prob", shape=(0, 1), chunks=(chunks_size, 1), dtype='float32',
+                                    overwrite=False, maxshape=(None, 1))
+        
         obs_group = root.create_group('observations')
         for key in obs_keys:
             if key not in obs_group:
@@ -73,13 +76,13 @@ def main(args):
         truncates = []
         timesteps = []
         model_names = []
-        infos = []
+        log_probs = []
         collisions = []
         print(f"Processing {file}")
         with open(os.path.join(args.input_folder, file), 'rb') as f:
             while True:
                 try:
-                    action, obs, reward, done, truncated, info, timestep, model_name, collision = pkl.load(f)
+                    action, obs, reward, done, truncated, log_prob, timestep, model_name, collision = pkl.load(f)
                     actions.append(action[0])
                     for key, value in obs.items():
                         if key == 'lidar_occupancy':
@@ -92,7 +95,7 @@ def main(args):
                     timesteps.append(timestep)
                     model_names.append(model_name)
                     collisions.append(collision)
-
+                    log_probs.append(log_prob)
 
                 except EOFError:
                     print("Done loading data")
@@ -116,6 +119,7 @@ def main(args):
             root["timestep"].append(np.array(timesteps))
             root["model_name"].append(np.array(model_names))
             root["collision"].append(np.array(collisions))
+            root["log_prob"].append(np.array(log_probs))
             print(np.array(model_names)[-1])
             print(root["model_name"][-1])
         else:
@@ -134,6 +138,7 @@ def main(args):
             root["timestep"] = timesteps
             root["model_name"] = model_names
             root["collision"] = np.array(collisions)
+            root["log_prob"] = np.array(log_probs)
         args.append= True
             
 if __name__ == "__main__":